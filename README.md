84be3a3b6a0ca879122e97f334c855dc6e93bb3c
Kevin Yang <chgc_kevin@hotmail.com>
2019-08-17 13:28:38 +0800

    update wallaby.jsremove nx support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Requirement
 
-- Angular CLI Version 6+
+- Angular CLI Version 8+
 
 # Getting Started With ngcli-wallaby Schematics
 
@@ -13,9 +13,7 @@ ng add ngcli-wallaby
 After running `ng add ngcli-wallaby` command, it will do following things.
 
 - Add the wallaby.js config file to the project.
-- Add the wallaby.js test bootstrap file to the project.
-- Exclude the src/wallabyTest.ts file in the tsconfig.json as it may affect Angular 2 AOT compilation.
-- Run npm install wallaby-webpack angular2-template-loader --save-dev.
+- Run npm install wallaby-webpack --save-dev.
 
 In default setup, it uses [Chrome (headless) runner](https://wallabyjs.com/docs/integration/chrome.html) as default runner instead electron. If you want to use electron, you can manually change it back by changing the env setting to env: {kind: 'electron'}, and run `npm i electron`
 
@@ -23,34 +21,7 @@ In this version, it only works on simple angular project. If you are using multi
 
 # Nrwl Nx project
 
-This schematics also works with NX project at first time.
-If there is more apps or libs created later on, it needs to manual update wallaby.js file. Steps as follow
-
-- Edit wallaby.js compilerOptions, add addition apps/libs tsconfig.spec.ts
-  ```typescript
-  var compilerOptions = Object.assign(
-    require('./tsconfig.json').compilerOptions
-    // Add here
-  );
-  ```
-- If it's libs, it also needs to update `resolve.alias` part
-  ```typescript
-  resolve: {
-      extensions: ['.js', '.ts'],
-      modules: [
-          path.join(wallaby.projectCacheDir, 'apps'),
-          path.join(wallaby.projectCacheDir, 'libs'),
-          'node_modules'
-      ],
-      // Add here
-      alias: {
-          '@myworkspacename/mylib': path.join(
-          wallaby.projectCacheDir,
-          'libs/mylib/src/index.ts'
-          )
-       }
-  },
-  ```
+Removed (2019/8/17)
 
 # Credit
 
@@ -59,4 +30,4 @@ Thanks to [TomWhiteOmni](https://github.com/TomWhiteOmni).
 # Reference
 
 - [ngCliWebpackSample](https://github.com/wallabyjs/ngCliWebpackSample)
-- [angular-nx-wallaby](https://github.com/TomWhiteOmni/angular-nx-wallaby)
+- ~~[angular-nx-wallaby](https://github.com/TomWhiteOmni/angular-nx-wallaby)~~
--- src/schematics/ng-add/index.ts
+++ src/schematics/ng-add/index.ts
@@ -3,14 +3,7 @@ import {
   Rule,
   SchematicContext,
   Tree,
-  url,
-  template,
-  apply,
-  mergeWith,
-  branchAndMerge,
-  SchematicsException,
-  move,
-  filter
+  url
 } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
@@ -18,10 +11,6 @@ const devDependencies = [
   {
     name: 'wallaby-webpack',
     version: '*'
-  },
-  {
-    name: 'angular2-template-loader',
-    version: '^0.6.2'
   }
 ];
 
@@ -37,15 +26,8 @@ interface ProjectList {
 
 export default function(_options: NgAddOptions): Rule {
   return (tree: Tree, context: SchematicContext) => {
-    const nx = containNxScematics(tree);
     return chain([
-      nx
-        ? chain([addBootstrapFileForNx(), addConfigFileForNx(tree)])
-        : chain([
-            addConfigFile(),
-            addBootstrapFile(),
-            modifyTsconfigExcludeFile()
-          ]),
+      chain([addConfigFile()]),
       addLoadersToPackageJson(),
       addPackageInstallTask()
     ])(tree, context);
@@ -66,39 +48,6 @@ function addConfigFile(): Rule {
       tree.create(wallabyConfigFileName, createContent);
     }
 
-    return tree;
-  };
-}
-
-function addBootstrapFile() {
-  return (tree: Tree, context: SchematicContext) => {
-    const wallabyBootstrapSourceFileName = '/src/wallabyTest.ts';
-    const wallabyBootstrapFileName = '/src/wallabyTest.ts';
-    const files: Tree = url(`../../files`)(context) as Tree;
-    const scriptContent = files
-      .read(wallabyBootstrapSourceFileName)!
-      .toString('utf-8');
-    if (!tree.exists(wallabyBootstrapFileName)) {
-      tree.create(wallabyBootstrapFileName, scriptContent);
-    }
-    return tree;
-  };
-}
-
-function modifyTsconfigExcludeFile(): Rule {
-  return (tree: Tree) => {
-    if (tree.exists('/tsconfig.app.json')) {
-      const tsConfigSource = tree.read('/tsconfig.app.json')!.toString('utf-8');
-      const json = JSON.parse(tsConfigSource);
-      if (json['exclude']) {
-        if (!(json['exclude'] as any[]).includes('src/wallabyTest.ts')) {
-          json['exclude'].push('src/wallabyTest.ts');
-        }
-      }
-      tree.overwrite('/tsconfig.app.json', JSON.stringify(json, null, 2));
-    } else {
-      throw new SchematicsException(`Could not find tsconfig.app.json.`);
-    }
     return tree;
   };
 }
@@ -159,93 +108,4 @@ export function addDependencyToPackageJson(
   }
 
   return tree;
-}
-
-/**
- * Work with Nx project
- */
-
-function addConfigFileForNx(tree: Tree): Rule {
-  const wallabyConfigSourceFileName = '/nx/wallaby/wallaby.js';
-  const importCompilerOptions = createCompilerOptions(getProjectLibs(tree));
-  const importAlias = createAlias(tree);
-  var templateSource = apply(url('../../files/nx/wallaby'), [
-    template({
-      importCompilerOptions,
-      importAlias
-    }),
-    move('nx/wallaby/', '')
-  ]);
-  return branchAndMerge(mergeWith(templateSource));
-}
-
-function addBootstrapFileForNx() {
-  const wallabyBootstrapSourceFileName = '/nx/wallabyTest/wallabyTest.ts';
-  var templateSource = apply(url('../../files/nx/wallabyTest'), [
-    move('nx/wallabyTest', '')
-  ]);
-  return branchAndMerge(mergeWith(templateSource));
-}
-
-function containNxScematics(tree: Tree): boolean {
-  const sourceText = tree.read('/package.json')!.toString('utf-8');
-  const packageJson = JSON.parse(sourceText);
-  return !!packageJson.dependencies['@nrwl/nx'];
-}
-
-function getProjectLibs(tree: Tree): ProjectList {
-  const sourceText = tree.read('/angular.json')!.toString('utf-8');
-  const packageJson = JSON.parse(sourceText);
-  let projects = { apps: [] as string[], libs: [] as string[] };
-  for (let project in packageJson.projects) {
-    if (project.includes('-e2e')) {
-      continue;
-    }
-    let projectInfo = packageJson.projects[project];
-    if (projectInfo.projectType === 'application') {
-      projects.apps.push(project);
-    }
-    if (projectInfo.projectType === 'library') {
-      projects.libs.push(project);
-    }
-  }
-  return projects;
-}
-
-function createCompilerOptions(defaultProject: ProjectList): string {
-  let compilerOptions: string[] = [];
-  compilerOptions = [
-    ...compilerOptions,
-    ...defaultProject.apps.map(
-      (app: string) =>
-        `require('./apps/${app}/tsconfig.spec.json').compilerOptions`
-    )
-  ];
-  compilerOptions = [
-    ...compilerOptions,
-    ...defaultProject.libs.map(
-      (lib: string) =>
-        `require('./libs/${lib}/tsconfig.spec.json').compilerOptions`
-    )
-  ];
-  return compilerOptions.join(',');
-}
-
-function createAlias(tree: Tree): string {
-  const sourceText = tree.read('/tsconfig.json')!.toString('utf-8');
-  const tsconfig = JSON.parse(sourceText);
-  let alias: any[] = [];
-  for (let path in tsconfig.compilerOptions.paths) {
-    if (!path.includes('*')) {
-      let value = tsconfig.compilerOptions.paths[path];
-      alias.push(`'${path}': path.join(wallaby.projectCacheDir, '${value}' )`);
-    }
-  }
-
-  if (alias.length === 0) {
-    return '';
-  }
-  return `alias: {
-    ${alias.join(',')}
-  }`;
 }
